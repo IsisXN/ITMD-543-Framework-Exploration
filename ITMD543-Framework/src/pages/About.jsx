8c9e7ec60400a9dc5246f5015ed6da9d6fafc4dc
XandraN09 <123044475+IsisXN@users.noreply.github.com>
2025-11-29 14:11:04 -0600

    Create About.jsx

---
+++ ITMD543-Framework/src/pages/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="page">
+      <h1>About Me</h1>
+      <p>I am a photographer passionate about capturing moments and telling visual stories.</p>
+    </div>
+  )
+}
+
