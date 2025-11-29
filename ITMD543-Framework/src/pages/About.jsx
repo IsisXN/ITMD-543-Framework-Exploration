9d57ebfcceaf0862277be069a91b8e8a217af60e
XandraN09 <123044475+IsisXN@users.noreply.github.com>
2025-11-29 16:18:32 -0600

    Update About.jsx

--- ITMD543-Framework/src/pages/About.jsx
+++ ITMD543-Framework/src/pages/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className="page">
       <h1>About Me</h1>
-      <p>Hello! I’m Xandra Nix, pursuing IT and technology-related studies. I enjoy working on projects that combine creativity with problem-solving, like this portfolio project.</p>
+      <p>My name is Isis N, pursuing a bachelors and masters ITM. I enjoy working on projects that combine creativity with problem-solving, like this portfolio project.</p>
       <h2>Resume Snapshot</h2>
       <ul>
         <li>Experience with React, Vite, and front-end web development</li>
