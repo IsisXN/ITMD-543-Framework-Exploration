5733c6847dc114c48070c54d704e53507de66dd8
XandraN09 <123044475+IsisXN@users.noreply.github.com>
2025-11-29 14:11:09 -0600

    Create Photography.jsx

---
+++ ITMD543-Framework/src/pages/Photography.jsx
@@ -0,0 +1,15 @@
+export default function Photography() {
+  return (
+    <div className="page">
+      <h1>Photography Gallery</h1>
+      <p>Here are some of my favorite shots:</p>
+
+      <div className="gallery">
+        <img src="https://via.placeholder.com/400x250" alt="Photo 1" />
+        <img src="https://via.placeholder.com/400x250" alt="Photo 2" />
+        <img src="https://via.placeholder.com/400x250" alt="Photo 3" />
+      </div>
+    </div>
+  )
+}
+
