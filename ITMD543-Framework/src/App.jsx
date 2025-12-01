99d149e4e9e384b86bbf3d952b1b9a04a3c845a1
XandraN09 <123044475+IsisXN@users.noreply.github.com>
2025-12-01 15:47:06 -0600

    Update App.jsx

--- ITMD543-Framework/src/App.jsx
+++ ITMD543-Framework/src/App.jsx
@@ -1,25 +1,25 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Photography from "./pages/Photography";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <header>
-        <nav className="navbar">
-          <Link to="/" className="logo">MyPortfolio</Link>
-          <ul className="nav-links">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/photography">Photography</Link></li>
-            <li><Link to="/about">About Me</Link></li>
-          </ul>
-        </nav>
-      </header>
+    <div>
+      {/* Navbar */}
+      <nav className="navbar-container">
+        <div className="navbar-logo">MyPortfolio</div>
+        <div className="navbar-links">
+          <Link to="/">Home</Link>
+          <Link to="/photography">Photography</Link>
+          <Link to="/about">About Me</Link>
+        </div>
+      </nav>
 
+      {/* Main content */}
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -27,8 +27,6 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </main>
-    </Router>
+    </div>
   );
 }
-
-export default App;
